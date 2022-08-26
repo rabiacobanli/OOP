974f570ff47bcb31aa7f9257a2814fa56b8d214c
Rabia ÇOBANLI <r.cobanli95@hotmail.com>
2022-08-26 16:11:46 +0300

    Add project files.

---
+++ OOP2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using OOP2;
+
+GercekMusteri musteri1=new GercekMusteri();
+musteri1.Id = 1;
+musteri1.MusteriNo = "123456";
+musteri1.TcNo = "5555555";
+musteri1.Adi = "Rabia";
+musteri1.Soyadi = "Çobanlı";
+
+
+TuzelMusteri musteri2=new TuzelMusteri();
+musteri2.Id = 2;
+musteri2.MusteriNo = "23456";
+musteri2.VergiNo = "123456";
+musteri2.SirketAdi = "Kodlama.io";
+
+
+Musteri musteri3=new GercekMusteri();
+Musteri musteri4 = new TuzelMusteri();
+
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.Ekle(musteri1);
+musteriManager.Ekle(musteri2);
+musteriManager.Ekle(musteri3);
+musteriManager.Ekle(musteri4);
